7699cd48a429d3061b4eaa90b6fa1384011f6ccc
vedantmhaske50-ship-it <vedantmhaske50@gmail.com>
2026-02-25 22:24:46 +0530

    Add files via upload

---
+++ Q7.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+   int main(){
+    int side,area;
+    printf("Enter Side : ");
+    scanf("%d",&side);
+
+    area=side*side;
+    printf("Area Of The Square Is : %d",area);
+    return 0;
+    
+
+   }
