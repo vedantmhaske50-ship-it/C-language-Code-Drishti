f0551b542708ba0c1de62874079e3dc6344bac02
vedantmhaske50-ship-it <vedantmhaske50@gmail.com>
2026-02-25 21:18:00 +0530

    Add files via upload

---
+++ Q1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+  
+   int main(){
+    int base,height,area;
+    printf("Enter Base : ");
+    scanf("%d",&base);
+    printf("Enter Height : ");
+    scanf("%d",&height);
+
+    area=0.5*base*height;
+    printf("Area Of The Triangle Is : %d",area);
+
+   }
