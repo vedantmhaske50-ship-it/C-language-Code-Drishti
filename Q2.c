85b8d6ae3bbcbcccf371253141de29e8eece3b8e
vedantmhaske50-ship-it <vedantmhaske50@gmail.com>
2026-02-25 21:35:47 +0530

    Add files via upload

---
+++ Q2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ 
+   int main(){
+    int length,width,area;
+    printf("Enter Length : ");
+    scanf("%d",&length);
+    printf("Enter Width : ");
+    scanf("%d",&width);
+
+    area=length*width;
+    printf("Area Of Rectangle Is : %d",area);
+
+   }
