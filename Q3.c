27cb7d4a8a9d97d0c492cc5b29c88c8509379816
vedantmhaske50-ship-it <vedantmhaske50@gmail.com>
2026-02-25 21:41:53 +0530

    Add files via upload

---
+++ Q3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ 
+   int main(){
+    int radius,area;
+    printf("Enter Radius : ");
+    scanf("%d",&radius);
+
+    area=3.14*radius*radius;
+    printf("Area Of Circle Is : %d",area);
+
+
+   }
